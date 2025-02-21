b9025f67e2bfe49a789738b8254ee0a42a1565be
ravi2krishna <ravi2krishna@gmail.com>
2025-02-21 11:28:37 +0530

    create variables for aws credentials

---
+++ tf-aws/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+  type        = string
+  description = "Enter AWS Access Key"
+}
+
+variable aws_secret_key {
+  type        = string
+  description = "Enter AWS Secret Access Key"
+}
