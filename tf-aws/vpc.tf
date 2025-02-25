7c739ea3216a2931a0e0b002dbd0cbbea4e74446
ravi2krishna <ravi2krishna@gmail.com>
2025-02-25 10:49:23 +0530

    create nacl

--- tf-aws/vpc.tf
+++ tf-aws/vpc.tf
@@ -90,3 +90,30 @@ resource "aws_route_table_association" "lms-db-asc" {
   subnet_id      = aws_subnet.lms-db-sn.id
   route_table_id = aws_route_table.lms-pvt-rt.id
 }
+
+# NACL
+resource "aws_network_acl" "lms-nacl" {
+  vpc_id = aws_vpc.lms.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = "lms-nacl"
+  }
+}
