3aa30a63130b6437086a2bc2d354a721189a319c
ravi2krishna <ravi2krishna@gmail.com>
2025-02-25 11:12:59 +0530

    create lms web server

--- tf-aws/vpc.tf
+++ tf-aws/vpc.tf
@@ -243,3 +243,16 @@ resource "aws_vpc_security_group_egress_rule" "lms-db-sg-all" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+# EC2 Web Server
+resource "aws_instance" "lms-web-server" {
+  ami           = "ami-06cff85354b67982b"
+  instance_type = "t2.micro"
+  key_name      = "2502"
+  subnet_id     = aws_subnet.lms-web-sn.id
+  vpc_security_group_ids = [aws_security_group.lms-web-sg.id]
+  
+  tags = {
+    Name = "lms-web-server"
+  }
+}
