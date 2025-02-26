a9c53a1cfe64c5553e6a9f04cb28e472d13afc5d
ravi2krishna <ravi2krishna@gmail.com>
2025-02-26 11:13:01 +0530

    create web nsg rules

--- tf-az/vnet.tf
+++ tf-az/vnet.tf
@@ -69,3 +69,33 @@ resource "azurerm_network_security_group" "lms-web-nsg" {
   location            = azurerm_resource_group.lms-rg.location
   resource_group_name = azurerm_resource_group.lms-rg.name
 }
+
+# Web Network Security Group - NSG - Rules
+resource "azurerm_network_security_rule" "lms-web-nsg-ssh" {
+  name                        = "ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.lms-rg.name
+  network_security_group_name = azurerm_network_security_group.lms-web-nsg.name
+}
+
+# Web Network Security Group - NSG - Rules
+resource "azurerm_network_security_rule" "lms-web-nsg-http" {
+  name                        = "http"
+  priority                    = 101
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.lms-rg.name
+  network_security_group_name = azurerm_network_security_group.lms-web-nsg.name
+}
