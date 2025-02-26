fdaa3d77eb943bfff450ca4292200e65a1581378
ravi2krishna <ravi2krishna@gmail.com>
2025-02-26 10:51:10 +0530

    create vnet

--- tf-az/vnet.tf
+++ tf-az/vnet.tf
@@ -4,3 +4,13 @@ resource "azurerm_resource_group" "lms-rg" {
   location = "East US"
 }
 
+# VNET
+resource "azurerm_virtual_network" "lms-vnet" {
+  name                = "lms"
+  location            = azurerm_resource_group.lms-rg.location
+  resource_group_name = azurerm_resource_group.lms-rg.name
+  address_space       = ["10.0.0.0/16"]
+    tags = {
+    environment = "dev"
+  }
+}
