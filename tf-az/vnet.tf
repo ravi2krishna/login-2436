be3fe2804cdef2b0962f29c840cc0eccebd692a9
ravi2krishna <ravi2krishna@gmail.com>
2025-02-27 10:52:07 +0530

    create api nic with nsg

--- tf-az/vnet.tf
+++ tf-az/vnet.tf
@@ -193,3 +193,23 @@ resource "azurerm_network_interface_security_group_association" "lms-web-nic-asc
   network_interface_id      = azurerm_network_interface.lms-web-nic.id
   network_security_group_id = azurerm_network_security_group.lms-web-nsg.id
 }
+
+# API NIC
+resource "azurerm_network_interface" "lms-api-nic" {
+  name                = "lms-api-nic"
+  location            = azurerm_resource_group.lms-rg.location
+  resource_group_name = azurerm_resource_group.lms-rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.lms-api-sn.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.lms-api-pip.id
+  }
+}
+
+# API NIC Association
+resource "azurerm_network_interface_security_group_association" "lms-api-nic-asc" {
+  network_interface_id      = azurerm_network_interface.lms-api-nic.id
+  network_security_group_id = azurerm_network_security_group.lms-api-nsg.id
+}
